997c0df3a929b88318571850c1c8bf4eed9b022e
Chew Chee Ming <cheeming@gmail.com>
2026-04-17 10:35:15 +0800

    Update shell.nix to make it work on macOS

--- cheeming.com-jekyll/shell.nix
+++ cheeming.com-jekyll/shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
-    sha256 = "11hcc63hbjiq935ldi9sqp5qri5ss3qli2029jxfrqi2796f1vqy";
+    sha256 = "0v6bd1xk8a2aal83karlvc853x44dg1n4nk08jg3dajqyy0s98np";
   };
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
@@ -9,12 +9,20 @@ in
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     ruby_3_2
+    llvmPackages_18.clang
+    llvmPackages_18.libcxx
     libffi
+    pkg-config
+    openssl
   ];
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
     pkgs.libffi
     pkgs.stdenv.cc.cc.lib
   ];
+
+  OPENSSL_DIR = "${pkgs.openssl.dev}";
+  OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+  OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
 }
 
